d3ce0989f39c07e24c53f9d97dda64a5addf6f6d
Rohan Maharjan <nahor.uzamaki@gmail.com>
2024-02-17 15:53:11 +0545

    program to calculate SI

---
+++ src/Lab0/Simpleinterest/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Simpleinterest
+{
+    internal class Program
+    {
+        static void Main(string[] args){
+            // program to calculate SI
+            float p = 100000, t = 10, r = 8, SI;
+
+            SI = (p * t * r)/100;
+            Console.WriteLine(SI);
+            Console.ReadLine();
+        }
+    }
+}
