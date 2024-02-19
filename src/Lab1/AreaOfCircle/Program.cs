3672c831b34a4fd67b01b59857cafc2c741e4271
Rohan Maharjan <nahor.uzamaki@gmail.com>
2024-02-19 17:25:34 +0545

    taking data from user to calculate the area of circle

---
+++ src/Lab1/AreaOfCircle/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace AreaOfCircle
+{
+    class Program{
+        static void Main(string[]  args){
+            double r, perimeter, area;
+            Console.WriteLine("Please enter the radius of circle :");
+
+            r = Convert.ToDouble(Console.ReadLine());
+            perimeter = 2 * 3.14 * r;
+            area = 3.14 * Math.Pow(r, 2); // area  =  3.14 * r * r;
+            Console.WriteLine("================================================");
+            Console.WriteLine("The perimeter of your circle: {0}", perimeter);
+            Console.WriteLine("The area of your circle: {0}", area);
+            Console.ReadLine();
+        }
+    }
+}
