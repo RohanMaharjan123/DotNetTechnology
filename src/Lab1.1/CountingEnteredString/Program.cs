906d143134bfc3de7d14648726b39b6f1d3fe482
Rohan Maharjan <nahor.uzamaki@gmail.com>
2024-02-19 17:46:22 +0545

    Program to count entered  string from user

---
+++ src/Lab1.1/CountingEnteredString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a string:");
+        string input = Console.ReadLine();
+
+        int characterCount = CountCharacters(input);
+
+        Console.WriteLine($"Number of characters entered: {characterCount}");
+    }
+
+    static int CountCharacters(string input)
+    {
+        // Use the Length property of the string to get the number of characters
+        return input.Length;
+    }
+}
