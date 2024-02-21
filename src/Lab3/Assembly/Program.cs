1970e7c7c1a2719785b1886ae180076d1cbbf165
Rohan Maharjan <nahor.uzamaki@gmail.com>
2024-02-21 19:13:43 +0545

    Assembly in C#

---
+++ src/Lab3/Assembly/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Assembly
+{
+    class Student
+    {
+        internal string name = "Shreean";
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Student thestudent = new Student();
+
+            Console.WriteLine("Name :" + thestudent.name);
+            Console.ReadLine();
+        }
+    }
+}
