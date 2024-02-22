1797c12e38f1446c982c59f6a86f9e88b2993337
Rohan Maharjan <nahor.uzamaki@gmail.com>
2024-02-23 00:51:24 +0545

    using static class and constructor in a program

---
+++ src/Lab6/StaticClassAndConstructor/Program.cs
@@ -0,0 +1,34 @@
+﻿//Example of static constructors and static class in C# 
+using System;
+namespace StaticClassAndConstructor
+{
+    public class Account
+    {
+        public int id;
+        public string name;
+        public static float rateOfInterest;
+        public Account(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+        static Account()
+        {
+            rateOfInterest = 9.5f;
+        }
+        public void display()
+        {
+            Console.WriteLine(id + " " + name + " " + rateOfInterest);
+        }
+        class TestEmployee
+        {
+            public static void Main(string[] args)
+            {
+                Account a1 = new Account(101, "Shristi");
+                Account a2 = new Account(102, "Shruti");
+                a1.display();
+                a2.display();
+            }
+        }
+    } 
+}
