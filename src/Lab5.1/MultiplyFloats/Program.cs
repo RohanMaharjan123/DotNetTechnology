cab5b3443e14aef267d0006548930dc056857e6d
Rohan Maharjan <nahor.uzamaki@gmail.com>
2024-02-26 22:49:46 +0545

    C# program to multiply float data entered by user

---
+++ src/Lab5.1/MultiplyFloats/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MultiplyFloat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number");
+            float first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            float second = Convert.ToInt32(Console.ReadLine());
+            float multiply = first * second;
+            Console.WriteLine("Result : {0}", multiply);
+            Console.ReadKey();
+        }
+    }
+}
