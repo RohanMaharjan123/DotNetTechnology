2388d82c8d9ce3d3d2aa527e561c0f7665defa42
Rohan Maharjan <nahor.uzamaki@gmail.com>
2024-02-28 14:13:59 +0545

    inheriting properties

---
+++ src/Lab2/Inheritance/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Bird
+{
+    public string Color { get; set; }
+
+    // Constructor
+    public Bird(string color)
+    {
+        Color = color;
+    }
+}
+
+class Display
+{
+    public static void DisplayBirdColor(string birdType, string color)
+    {
+        Console.WriteLine($"Color of {birdType}: {color}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create instances of Bird class for crow, pigeon, and penguin
+        Bird crow = new Bird("Black");
+        Bird pigeon = new Bird("Gray");
+        Bird penguin = new Bird("Black and White");
+
+        // Display colors of crow, pigeon, and penguin using Display class
+        Display.DisplayBirdColor("Crow", crow.Color);
+        Display.DisplayBirdColor("Pigeon", pigeon.Color);
+        Display.DisplayBirdColor("Penguin", penguin.Color);
+    }
+}
