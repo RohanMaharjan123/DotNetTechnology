29c5137beda09c49b1cfb47c2da363ed9da0c943
Rohan Maharjan <nahor.uzamaki@gmail.com>
2024-03-03 18:48:27 +0545

    Generics in C#

---
+++ src/Lab9/Generics/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+// Generic class definition for a pair of values
+public class Pair<T>
+{
+    public T First { get; set; }
+    public T Second { get; set; }
+
+    // Constructor
+    public Pair(T first, T second)
+    {
+        First = first;
+        Second = second;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create a Pair of integers
+        Pair<int> intPair = new Pair<int>(10, 20);
+        Console.WriteLine($"First: {intPair.First}, Second: {intPair.Second}");
+
+        // Create a Pair of strings
+        Pair<string> stringPair = new Pair<string>("Hello", "World");
+        Console.WriteLine($"First: {stringPair.First}, Second: {stringPair.Second}");
+    }
+}
